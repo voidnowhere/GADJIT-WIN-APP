75c4e5c495f0e5569e344566857d11c2ed385267
voidnowhere <ali00almai@gmail.com>
2021-05-01 17:12:25 +0000

    login for CLIENT and ASW

--- GADJIT-WIN-CLIENT/Program.cs
+++ GADJIT-WIN-CLIENT/Program.cs
@@ -16,7 +16,7 @@ namespace GADJIT_WIN_CLIENT
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
