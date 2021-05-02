454151d54ef523a6a6741150c7bb338592aaa794
voidnowhere <ali00almai@gmail.com>
2021-05-02 00:44:35 +0000

    email cin Validater | ID Generator

--- GADJIT-WIN-ASW/GADJIT.cs
+++ GADJIT-WIN-ASW/GADJIT.cs
@@ -4,11 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 namespace GADJIT_WIN_ASW
 {
     static class GADJIT
     {
         public static SqlConnection sqlConnection = new SqlConnection(@"Server=.\SQLEXPRESS; Database=GADJIT; User Id=gadjit_basic; Password=g_2021;");
+
+        public static string IDGenerator(string id)
+        {
+            String code = Regex.Match(id, @"^\D+").ToString();
+            String num = Regex.Match(id, @"\d+$").ToString();
+
+            return code + (int.Parse(num) + 1).ToString();
+        }
+
+        public static bool IsCINValid(string cin)
+        {
+            if (Regex.IsMatch(cin, @"^[A-Z]{1,2}\d{6}$"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsEmailValid(string email)
+        {
+            if (Regex.IsMatch(email, @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"))
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
