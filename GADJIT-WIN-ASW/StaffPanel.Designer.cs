14b826779d0ac9cd29e5318cf4bb8607302a9717
AkaMoughit <73391568+AkaMoughit@users.noreply.github.com>
2021-05-03 23:27:48 +0000

    Gadgit Win app for ASW Login done Maquette of Staff and Worker Panel are done Disponibility systeme Done

---
+++ GADJIT-WIN-ASW/StaffPanel.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace GADJIT_WIN_ASW
+{
+    partial class StaffPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.ButtonTicketProgression = new System.Windows.Forms.Button();
+            this.ButtonTicketVerification = new System.Windows.Forms.Button();
+            this.ShowSubMenuButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.LabelEmail = new System.Windows.Forms.Label();
+            this.LabelFirstName = new System.Windows.Forms.Label();
+            this.LabelLastName = new System.Windows.Forms.Label();
+            this.LogoutButton = new GADJIT_WIN_ASW.CircularPicture();
+            this.CircularProfilPicture = new GADJIT_WIN_ASW.CircularPicture();
+            this.cirucularButton = new GADJIT_WIN_ASW.CirucularButton();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoutButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CircularProfilPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Controls.Add(this.LogoutButton);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.ShowSubMenuButton);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 708);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.ButtonTicketProgression);
+            this.panel3.Controls.Add(this.ButtonTicketVerification);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 133);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(250, 82);
+            this.panel3.TabIndex = 2;
+            // 
+            // ButtonTicketProgression
+            // 
+            this.ButtonTicketProgression.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ButtonTicketProgression.FlatAppearance.BorderSize = 0;
+            this.ButtonTicketProgression.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonTicketProgression.ForeColor = System.Drawing.Color.DarkCyan;
+            this.ButtonTicketProgression.Location = new System.Drawing.Point(0, 40);
+            this.ButtonTicketProgression.Name = "ButtonTicketProgression";
+            this.ButtonTicketProgression.Size = new System.Drawing.Size(250, 40);
+            this.ButtonTicketProgression.TabIndex = 1;
+            this.ButtonTicketProgression.Text = "Ticket Progression";
+            this.ButtonTicketProgression.UseVisualStyleBackColor = true;
+            // 
+            // ButtonTicketVerification
+            // 
+            this.ButtonTicketVerification.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ButtonTicketVerification.FlatAppearance.BorderSize = 0;
+            this.ButtonTicketVerification.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonTicketVerification.ForeColor = System.Drawing.Color.DarkCyan;
+            this.ButtonTicketVerification.Location = new System.Drawing.Point(0, 0);
+            this.ButtonTicketVerification.Name = "ButtonTicketVerification";
+            this.ButtonTicketVerification.Size = new System.Drawing.Size(250, 40);
+            this.ButtonTicketVerification.TabIndex = 0;
+            this.ButtonTicketVerification.Text = "Ticket Verification";
+            this.ButtonTicketVerification.UseVisualStyleBackColor = true;
+            // 
+            // ShowSubMenuButton
+            // 
+            this.ShowSubMenuButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ShowSubMenuButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ShowSubMenuButton.FlatAppearance.BorderSize = 0;
+            this.ShowSubMenuButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ShowSubMenuButton.ForeColor = System.Drawing.Color.White;
+            this.ShowSubMenuButton.Location = new System.Drawing.Point(0, 93);
+            this.ShowSubMenuButton.Name = "ShowSubMenuButton";
+            this.ShowSubMenuButton.Size = new System.Drawing.Size(250, 40);
+            this.ShowSubMenuButton.TabIndex = 1;
+            this.ShowSubMenuButton.Text = "Gestion Ticket";
+            this.ShowSubMenuButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ShowSubMenuButton.UseCompatibleTextRendering = true;
+            this.ShowSubMenuButton.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.CircularProfilPicture);
+            this.panel2.Controls.Add(this.cirucularButton);
+            this.panel2.Controls.Add(this.LabelEmail);
+            this.panel2.Controls.Add(this.LabelFirstName);
+            this.panel2.Controls.Add(this.LabelLastName);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(250, 93);
+            this.panel2.TabIndex = 0;
+            // 
+            // LabelEmail
+            // 
+            this.LabelEmail.AutoSize = true;
+            this.LabelEmail.ForeColor = System.Drawing.Color.White;
+            this.LabelEmail.Location = new System.Drawing.Point(12, 63);
+            this.LabelEmail.Name = "LabelEmail";
+            this.LabelEmail.Size = new System.Drawing.Size(79, 17);
+            this.LabelEmail.TabIndex = 6;
+            this.LabelEmail.Text = "Staff  Email";
+            // 
+            // LabelFirstName
+            // 
+            this.LabelFirstName.AutoSize = true;
+            this.LabelFirstName.ForeColor = System.Drawing.Color.White;
+            this.LabelFirstName.Location = new System.Drawing.Point(12, 36);
+            this.LabelFirstName.Name = "LabelFirstName";
+            this.LabelFirstName.Size = new System.Drawing.Size(108, 17);
+            this.LabelFirstName.TabIndex = 5;
+            this.LabelFirstName.Text = "Staff First Name";
+            // 
+            // LabelLastName
+            // 
+            this.LabelLastName.AutoSize = true;
+            this.LabelLastName.ForeColor = System.Drawing.Color.White;
+            this.LabelLastName.Location = new System.Drawing.Point(12, 9);
+            this.LabelLastName.Name = "LabelLastName";
+            this.LabelLastName.Size = new System.Drawing.Size(106, 17);
+            this.LabelLastName.TabIndex = 4;
+            this.LabelLastName.Text = "Staff Last Name";
+            // 
+            // LogoutButton
+            // 
+            this.LogoutButton.Image = global::GADJIT_WIN_ASW.Properties.Resources.logout_rounded_left_48pxblue;
+            this.LogoutButton.Location = new System.Drawing.Point(0, 658);
+            this.LogoutButton.Name = "LogoutButton";
+            this.LogoutButton.Size = new System.Drawing.Size(50, 50);
+            this.LogoutButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LogoutButton.TabIndex = 3;
+            this.LogoutButton.TabStop = false;
+            this.LogoutButton.Click += new System.EventHandler(this.LogoutButton_Click);
+            // 
+            // CircularProfilPicture
+            // 
+            this.CircularProfilPicture.Image = global::GADJIT_WIN_ASW.Properties.Resources.user_60px;
+            this.CircularProfilPicture.Location = new System.Drawing.Point(187, 3);
+            this.CircularProfilPicture.Name = "CircularProfilPicture";
+            this.CircularProfilPicture.Size = new System.Drawing.Size(60, 60);
+            this.CircularProfilPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CircularProfilPicture.TabIndex = 8;
+            this.CircularProfilPicture.TabStop = false;
+            // 
+            // cirucularButton
+            // 
+            this.cirucularButton.BackColor = System.Drawing.Color.Lime;
+            this.cirucularButton.FlatAppearance.BorderSize = 2;
+            this.cirucularButton.Location = new System.Drawing.Point(223, 67);
+            this.cirucularButton.Name = "cirucularButton";
+            this.cirucularButton.Size = new System.Drawing.Size(24, 23);
+            this.cirucularButton.TabIndex = 7;
+            this.cirucularButton.UseVisualStyleBackColor = false;
+            this.cirucularButton.Click += new System.EventHandler(this.cirucularButton_Click);
+            // 
+            // StaffPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1377, 708);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "StaffPanel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "StaffPanel";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.StaffPanel_FormClosing);
+            this.Load += new System.EventHandler(this.StaffPanel_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoutButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CircularProfilPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button ButtonTicketProgression;
+        private System.Windows.Forms.Button ButtonTicketVerification;
+        private System.Windows.Forms.Button ShowSubMenuButton;
+        private System.Windows.Forms.Panel panel2;
+        private CirucularButton cirucularButton;
+        private CircularPicture LogoutButton;
+        public System.Windows.Forms.Label LabelEmail;
+        public System.Windows.Forms.Label LabelFirstName;
+        public System.Windows.Forms.Label LabelLastName;
+        public CircularPicture CircularProfilPicture;
+    }
+}
---
+++ GADJIT-WIN-ASW/StaffPanel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace GADJIT_WIN_ASW
+{
+    public partial class StaffPanel : Form
+    {
+        public StaffPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void StaffPanel_Load(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("UPDATE Staff SET StafDispo='En Ligne' where StafEmail=@Email", GADJIT.sqlConnection);
+            cmd.Parameters.AddWithValue("@Email", LabelEmail.Text);
+            GADJIT.sqlConnection.Open();
+            cmd.ExecuteNonQuery();
+            GADJIT.sqlConnection.Close();
+        }
+
+        private void cirucularButton_Click(object sender, EventArgs e)
+        {
+            GADJIT.sqlConnection.Open();
+            if (cirucularButton.BackColor == Color.Lime)
+            {
+                cirucularButton.BackColor = Color.Orange;
+                SqlCommand cmd = new SqlCommand("UPDATE Staff SET StafDispo='Break' where StafEmail=@Email", GADJIT.sqlConnection);
+                cmd.Parameters.AddWithValue("@Email", LabelEmail.Text);
+                cmd.ExecuteNonQuery();        
+            }
+            else
+            {
+                cirucularButton.BackColor = Color.Lime;
+                SqlCommand cmd = new SqlCommand("UPDATE Staff SET StafDispo='En Ligne' where StafEmail=@Email", GADJIT.sqlConnection);
+                cmd.Parameters.AddWithValue("@Email", LabelEmail.Text);
+                cmd.ExecuteNonQuery();
+            }
+            GADJIT.sqlConnection.Close();
+        }
+
+        private void LogoutButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void StaffPanel_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            GADJIT.sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand("UPDATE Staff SET StafDispo='Hors Ligne' where StafEmail=@Email", GADJIT.sqlConnection);
+            cmd.Parameters.AddWithValue("@Email", LabelEmail.Text);
+            GADJIT.sqlConnection.Open();
+            cmd.ExecuteNonQuery();
+            GADJIT.sqlConnection.Close();
+        }
+    }
+}
