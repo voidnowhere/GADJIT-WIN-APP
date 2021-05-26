02926de053a9a3be17d5b226cde33d9bf0744772
AkaMoughit <73391568+AkaMoughit@users.noreply.github.com>
2021-05-26 15:30:39 +0100

    Login fixed

--- GADJIT-WIN-ASW/Login.Designer.cs
+++ GADJIT-WIN-ASW/Login.Designer.cs
@@ -155,7 +155,7 @@ namespace GADJIT_WIN_ASW
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1026, 494);
+            this.ClientSize = new System.Drawing.Size(1026, 492);
             this.Controls.Add(this.LabelHelp);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
