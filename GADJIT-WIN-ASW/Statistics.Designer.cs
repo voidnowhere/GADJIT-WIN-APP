367827a02c9e4a745d4ef36ec6ccce246bedadbf
voidnowhere <ali00almai@gmail.com>
2021-06-03 21:52:07 +0100

    AdminPanel Statistics

---
+++ GADJIT-WIN-ASW/Statistics.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace GADJIT_WIN_ASW
+{
+    partial class Statistics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CrystalReportViewer = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RadioButtonWorkerStats = new System.Windows.Forms.RadioButton();
+            this.RadioButtonGadgetStats = new System.Windows.Forms.RadioButton();
+            this.DTPFrom = new System.Windows.Forms.DateTimePicker();
+            this.DTPTo = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CrystalReportViewer
+            // 
+            this.CrystalReportViewer.ActiveViewIndex = -1;
+            this.CrystalReportViewer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CrystalReportViewer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CrystalReportViewer.Cursor = System.Windows.Forms.Cursors.Default;
+            this.CrystalReportViewer.Location = new System.Drawing.Point(12, 123);
+            this.CrystalReportViewer.Name = "CrystalReportViewer";
+            this.CrystalReportViewer.ShowCloseButton = false;
+            this.CrystalReportViewer.ShowGroupTreeButton = false;
+            this.CrystalReportViewer.ShowLogo = false;
+            this.CrystalReportViewer.ShowParameterPanelButton = false;
+            this.CrystalReportViewer.ShowRefreshButton = false;
+            this.CrystalReportViewer.ShowTextSearchButton = false;
+            this.CrystalReportViewer.Size = new System.Drawing.Size(648, 389);
+            this.CrystalReportViewer.TabIndex = 0;
+            this.CrystalReportViewer.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(274, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Statistiques";
+            // 
+            // RadioButtonWorkerStats
+            // 
+            this.RadioButtonWorkerStats.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RadioButtonWorkerStats.AutoSize = true;
+            this.RadioButtonWorkerStats.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RadioButtonWorkerStats.Location = new System.Drawing.Point(570, 59);
+            this.RadioButtonWorkerStats.Name = "RadioButtonWorkerStats";
+            this.RadioButtonWorkerStats.Size = new System.Drawing.Size(92, 20);
+            this.RadioButtonWorkerStats.TabIndex = 2;
+            this.RadioButtonWorkerStats.TabStop = true;
+            this.RadioButtonWorkerStats.Text = "Technicien";
+            this.RadioButtonWorkerStats.UseVisualStyleBackColor = true;
+            this.RadioButtonWorkerStats.Click += new System.EventHandler(this.RadioButtonWorkerStats_Click);
+            // 
+            // RadioButtonGadgetStats
+            // 
+            this.RadioButtonGadgetStats.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RadioButtonGadgetStats.AutoSize = true;
+            this.RadioButtonGadgetStats.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RadioButtonGadgetStats.Location = new System.Drawing.Point(570, 93);
+            this.RadioButtonGadgetStats.Name = "RadioButtonGadgetStats";
+            this.RadioButtonGadgetStats.Size = new System.Drawing.Size(71, 20);
+            this.RadioButtonGadgetStats.TabIndex = 3;
+            this.RadioButtonGadgetStats.TabStop = true;
+            this.RadioButtonGadgetStats.Text = "Gadget";
+            this.RadioButtonGadgetStats.UseVisualStyleBackColor = true;
+            this.RadioButtonGadgetStats.Click += new System.EventHandler(this.RadioButtonGadgetStats_Click);
+            // 
+            // DTPFrom
+            // 
+            this.DTPFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DTPFrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.DTPFrom.Location = new System.Drawing.Point(38, 28);
+            this.DTPFrom.Name = "DTPFrom";
+            this.DTPFrom.Size = new System.Drawing.Size(87, 22);
+            this.DTPFrom.TabIndex = 4;
+            // 
+            // DTPTo
+            // 
+            this.DTPTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DTPTo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.DTPTo.Location = new System.Drawing.Point(153, 28);
+            this.DTPTo.Name = "DTPTo";
+            this.DTPTo.Size = new System.Drawing.Size(87, 22);
+            this.DTPTo.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.DTPFrom);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.DTPTo);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Goldenrod;
+            this.groupBox1.Location = new System.Drawing.Point(12, 48);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(552, 69);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(131, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(6, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "De";
+            // 
+            // Statistics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 524);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.RadioButtonGadgetStats);
+            this.Controls.Add(this.RadioButtonWorkerStats);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CrystalReportViewer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Statistics";
+            this.Text = "Statistics";
+            this.Load += new System.EventHandler(this.Statistics_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer CrystalReportViewer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton RadioButtonWorkerStats;
+        private System.Windows.Forms.RadioButton RadioButtonGadgetStats;
+        private System.Windows.Forms.DateTimePicker DTPFrom;
+        private System.Windows.Forms.DateTimePicker DTPTo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ GADJIT-WIN-ASW/Statistics.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GADJIT_WIN_ASW
+{
+    public partial class Statistics : Form
+    {
+        public Statistics()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadWorkerStatsReport()
+        {
+            try
+            {
+                CrystalReportWorkerStats crystalReportWorkerStats = new CrystalReportWorkerStats();
+                crystalReportWorkerStats.SetDatabaseLogon("gadjit_basic", "cz3l@K$H%!W2", "pff-win-app.database.windows.net", "GADJIT");
+                crystalReportWorkerStats.SetParameterValue("from", DTPFrom.Value.ToShortDateString());
+                crystalReportWorkerStats.SetParameterValue("to", DateTime.Parse(DateTime.Now.ToShortDateString()).AddHours(23).AddMinutes(59).AddSeconds(59).ToString());
+                crystalReportWorkerStats.SetParameterValue("toDate", DateTime.Now.ToShortDateString());
+                CrystalReportViewer.ReportSource = crystalReportWorkerStats;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error LoadWorkerStatsReport()", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LoadGadgetStatsReport()
+        {
+            try
+            {
+                CrystalReportGadgetStats crystalReportGadgetStats = new CrystalReportGadgetStats();
+                crystalReportGadgetStats.SetDatabaseLogon("gadjit_basic", "cz3l@K$H%!W2", "pff-win-app.database.windows.net", "GADJIT");
+                crystalReportGadgetStats.SetParameterValue("from", DTPFrom.Value.ToShortDateString());
+                crystalReportGadgetStats.SetParameterValue("to", DateTime.Parse(DateTime.Now.ToShortDateString()).AddHours(23).AddMinutes(59).AddSeconds(59).ToString());
+                crystalReportGadgetStats.SetParameterValue("toDate", DateTime.Now.ToShortDateString());
+                CrystalReportViewer.ReportSource = crystalReportGadgetStats;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error LoadGadgetStatsReport()", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Statistics_Load(object sender, EventArgs e)
+        {
+            DTPFrom.MaxDate = DateTime.Parse(DateTime.Now.AddDays(-1).ToShortDateString());
+            DTPTo.MaxDate = DateTime.Parse(DateTime.Now.ToShortDateString()).AddHours(23).AddMinutes(59).AddSeconds(59);
+            RadioButtonWorkerStats.Checked = true;
+            LoadWorkerStatsReport();
+        }
+
+        private void RadioButtonWorkerStats_Click(object sender, EventArgs e)
+        {
+            if (RadioButtonWorkerStats.Checked)
+            {
+                LoadWorkerStatsReport();
+            }
+        }
+
+        private void RadioButtonGadgetStats_Click(object sender, EventArgs e)
+        {
+            if (RadioButtonGadgetStats.Checked)
+            {
+                LoadGadgetStatsReport();
+            }
+        }
+    }
+}
